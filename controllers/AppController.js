3d1dcf4d86ca1518426d009a644822a708d4267f
Noah Too <krivahtoo@gmail.com>
2024-12-11 22:21:15 +0300

    Add Tasks 3. Create a new user

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,19 +1,19 @@
-import redisClient from '../utils/redis.js';
-import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AppController {
-    static getStatus(req, res) {
-        res.status(200).json({
-            redis: redisClient.isAlive(),
-            db: dbClient.isAlive(),
-        });
-    }
+  static getStatus(req, res) {
+    return res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
 
-    static async getStats(req, res) {
-        const users = await dbClient.nbUsers();
-        const files = await dbClient.nbFiles();
-        res.status(200).json({ users, files });
-    }
+  static async getStats(req, res) {
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    return res.status(200).json({ users, files });
+  }
 }
 
 export default AppController;
