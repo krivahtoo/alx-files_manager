86c78c44d7e4dfd9e8514c4096a1a80bd07596a7
Noah Too <krivahtoo@gmail.com>
2024-12-11 20:16:12 +0300

    Add Tasks 0. Redis utils and 1. MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,37 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.dbName = database;
+    this.client.connect().catch((err) => {
+      console.error('MongoDB connection error:', err);
+    });
+  }
+
+  isAlive() {
+    return this.client && this.client.isConnected();
+  }
+
+  async nbUsers() {
+    if (!this.isAlive()) return 0;
+    const db = this.client.db(this.dbName);
+    const usersCollection = db.collection('users');
+    return usersCollection.countDocuments();
+  }
+
+  async nbFiles() {
+    if (!this.isAlive()) return 0;
+    const db = this.client.db(this.dbName);
+    const filesCollection = db.collection('files');
+    return filesCollection.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
